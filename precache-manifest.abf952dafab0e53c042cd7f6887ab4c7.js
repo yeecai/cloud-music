077d585c885f60c6015f30057981bdd7611e9952
yeecai <yushunchn@gmail.com>
2020-05-11 15:43:16 +0800

    Updates

---
+++ precache-manifest.abf952dafab0e53c042cd7f6887ab4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7144f275b566caa4d5b6bef40c719579",
+    "url": "/cloud-music/index.html"
+  },
+  {
+    "revision": "03b11776a99ae846ab28",
+    "url": "/cloud-music/static/js/2.c78c12c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cloud-music/static/js/2.c78c12c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ea2c2f355879df6c88",
+    "url": "/cloud-music/static/js/main.981b313e.chunk.js"
+  },
+  {
+    "revision": "3219f8b0e12e5e07ed2f",
+    "url": "/cloud-music/static/js/runtime-main.71420d50.js"
+  }
+]);
