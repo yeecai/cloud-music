bb7fa812fcfa5bbdd972488bcb05fee0371835c1
yeecai <yushunchn@gmail.com>
2020-05-21 15:55:53 +0800

    Updates

--- precache-manifest.5be4a91cc747a3efd469353931b33f7d.js
+++ precache-manifest.5be4a91cc747a3efd469353931b33f7d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf6f2f8fd3a64bbf51654a526a19de68",
+    "revision": "23cdb80b6fd3e1c15a6b146db90963f8",
     "url": "/cloud-music/index.html"
   },
   {
-    "revision": "d4a8f18c2afaa51b1454",
+    "revision": "1573c6fd8c69ac5da2ba",
     "url": "/cloud-music/static/css/2.508070a3.chunk.css"
   },
   {
-    "revision": "d4a8f18c2afaa51b1454",
-    "url": "/cloud-music/static/js/2.0d3f0565.chunk.js"
+    "revision": "1573c6fd8c69ac5da2ba",
+    "url": "/cloud-music/static/js/2.387bb84f.chunk.js"
   },
   {
     "revision": "2db6a849239548a98b7bbdb7e39c2942",
-    "url": "/cloud-music/static/js/2.0d3f0565.chunk.js.LICENSE.txt"
+    "url": "/cloud-music/static/js/2.387bb84f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4479c907b5c6ef112d1a",
-    "url": "/cloud-music/static/js/main.553772d9.chunk.js"
+    "revision": "fbe0c648a37728bed26e",
+    "url": "/cloud-music/static/js/main.99f1e4a0.chunk.js"
   },
   {
     "revision": "3219f8b0e12e5e07ed2f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cloud-music/precache-manifest.6ba72638ed4fb6f9528987ad992a469b.js"
+  "/cloud-music/precache-manifest.5be4a91cc747a3efd469353931b33f7d.js"
 );
 
 self.addEventListener('message', (event) => {
