65cc1cec8dec70caceccda9410e775b103e32bfe
ye cai <yushunchn@gmail.com>
2020-07-06 20:50:11 +0800

    fixed scroll bug, typo again = =

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React , {useRef} from "react";
 import Slider from "../../components/slider";
-import RecommendList from '../../components/list'
-import Scroll from '../../baseUI/scroll';
-import Loading from '../../baseUI/loading'
-import { Content } from './style';
+import RecommendList from "../../components/list";
+import Scroll from "../../baseUI/scroll";
+import Loading from "../../baseUI/loading";
+import { Content } from "./style";
 
-import { connect } from 'react-redux'
-import * as actionTypes from './store/actionCreators'
-import { useEffect } from 'react';
-import { forceCheck } from 'react-lazyload';
-import { renderRoutes } from 'react-router-config';
+import { connect } from "react-redux";
+import * as actionTypes from "./store/actionCreators";
+import { useEffect } from "react";
+import { forceCheck } from "react-lazyload";
+import { renderRoutes } from "react-router-config";
 
 // debugger
 function Recommend(props) {
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
-  const { bannerList, recommendList, isLoading } = props
+  const { bannerList, recommendList, isLoading, songsCount } = props;
 
   useEffect(() => {
     if (!bannerList.size) {
@@ -23,43 +23,45 @@ function Recommend(props) {
     if (!recommendList.size) {
       getRecommendListDataDispatch();
     }
-  }, [])
+  }, []);
 
   const bannerListJS = bannerList ? bannerList.toJS() : [];
   const recommendListJS = recommendList ? recommendList.toJS() : [];
-
+  const scrollRef = useRef();
   return (
-    <Content>
-      <Scroll className="list" 
-      onScroll={forceCheck}
-      >
+    <Content  play={songsCount}>
+      <Scroll className="list" ref={scrollRef} onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
-      {isLoading ? <Loading></Loading>: null}
-      { renderRoutes(props.route.routes) }
+      {isLoading ? <Loading></Loading> : null}
+      {renderRoutes(props.route.routes)}
     </Content>
-  )
+  );
 }
 
 const mapStateToProps = (state) => ({
-  //dont toJS data here 
+  //dont toJS data here
   // otherwise cause extra re-render
-  bannerList: state.getIn(['recommend', 'bannerList']),
-  recommendList: state.getIn(['recommend', 'recommendList']),
-  isLoading: state.getIn(['recommend', 'isLoading'])
-})
+  bannerList: state.getIn(["recommend", "bannerList"]),
+  recommendList: state.getIn(["recommend", "recommendList"]),
+  isLoading: state.getIn(["recommend", "isLoading"]),
+  songsCount: state.getIn(['player', 'playList']).size,//尽量减少toJS操作，直接取size属性就代表了list的长度
+});
 
 const mapDispatchToProps = (dispatch) => {
   return {
     getBannerDataDispatch() {
-      dispatch(actionTypes.getBannerList())
+      dispatch(actionTypes.getBannerList());
     },
     getRecommendListDataDispatch() {
-      dispatch(actionTypes.getRecommendList())
-    }
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend));
+      dispatch(actionTypes.getRecommendList());
+    },
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(React.memo(Recommend));
--- src/baseUI/scroll/index.js
+++ src/baseUI/scroll/index.js
@@ -1,21 +1,28 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef, useState, useMemo } from 'react'
-import BScroll from "better-scroll"
-import PropTypes from "prop-types"
-import styled from 'styled-components'
-import Loading from '../loading'
-import LoadingV2 from '../loading-v2'
-import { debounce } from '../../api/utils'
-
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+  useMemo,
+} from "react";
+import BScroll from "better-scroll";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Loading from "../loading";
+import LoadingV2 from "../loading-v2";
+import { debounce } from "../../api/utils";
 
 const ScrollContainer = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
-`
+`;
 
 const PullupLoading = styled.div`
   position: absolute;
-  left:0; right:0;
+  left: 0;
+  right: 0;
   bottom: 5px;
   width: 60px;
   height: 60px;
@@ -23,9 +30,10 @@ const PullupLoading = styled.div`
   z-index: 100;
 `;
 
-const PulldownLoading = styled.div`
+const PullDownLoading = styled.div`
   position: absolute;
-  left:0; right:0;
+  left: 0;
+  right: 0;
   top: 0px;
   height: 30px;
   margin: auto;
@@ -33,137 +41,131 @@ const PulldownLoading = styled.div`
 `;
 
 const Scroll = forwardRef((props, ref) => {
-    const scrollContaninerRef = useRef();
+  const scrollContaninerRef = useRef();
 
-    const { direction, click, refresh, bounceTop, bounceBottom } = props;
-    const { pullUp, pullDown, onScroll, pullupLoading, pulldownLoading } = props;
+  const { direction, click, refresh, bounceTop, bounceBottom } = props;
+  const { pullUp, pullDown, onScroll, pullUpLoading, pullDownLoading } = props;
 
-    const [bScroll, setBScroll] = useState();
+  const [bScroll, setBScroll] = useState();
 
-    let pullUpDebounce = useMemo(() => {
-        return debounce(pullUp, 300)
-    }, [pullUp])
+  let pullUpDebounce = useMemo(() => {
+    return debounce(pullUp, 300);
+  }, [pullUp]);
 
-    let pullDownDebounce = useMemo(() => {
-        return debounce(pullDown, 300)
-    }, [pullDown])
+  let pullDownDebounce = useMemo(() => {
+    return debounce(pullDown, 300);
+  }, [pullDown]);
 
-    useEffect(() => {
-        const scroll = new BScroll(scrollContaninerRef.current, {
-            scrollX: direction === 'horizental',
-            scrollY: direction === 'vertical',
-            propeType: 3,
-            click: click,
-            bounce: {
-                top: bounceTop,
-                bottom: bounceBottom
-            }
-        });
-
-        setBScroll(scroll);
-        return () => {
-            setBScroll(null)
-        }
-    }, [])
-
-
-    useEffect(() => {
-        if (!bScroll || !onScroll) return;
-        bScroll.on('scroll', onScroll)
-        return () => {
-            bScroll.off('scroll', onScroll)
-        }
-    }, [onScroll, bScroll])
-
-    useEffect(() => {
-        if (!bScroll || !pullUp) return;
-        const handlePullUp = () => {
-            //判断是否滑动到了底部
-            if(bScroll.y <= bScroll.maxScrollY + 100){
-              pullUpDebounce();
-            }
-          }
-        // bScroll.on('scrollEnd', () => {
-        //     if (bScroll.y <= bScroll.maxScrollY + 100) {
-        //         pullUp();
-        //     }
-        // })
-        bScroll.on('scrollEnd', handlePullUp);
-        return () => {
-            bScroll.off('scrollEnd')
-        }
-    }, [pullUp, pullUpDebounce, bScroll])
-
-    useEffect(() => {
-        if (!bScroll || !pullDown) return;
-        bScroll.on('touchEnd', (pos) => {
-            if (pos.y > 50) {
-                pullDown()
-            }
-        })
-
-        return () => {
-            bScroll.off('touchEnd')
-        }
-    }, [pullDown, pullDownDebounce, bScroll])
-
-    useEffect(() => {
-        if (refresh && bScroll) {
-            bScroll.refresh();
-        }
+  useEffect(() => {
+    const scroll = new BScroll(scrollContaninerRef.current, {
+      scrollX: direction === "horizental",
+      scrollY: direction === "vertical",
+      probeType: 3,
+      click: click,
+      bounce: {
+        top: bounceTop,
+        bottom: bounceBottom,
+      },
     });
 
-    useImperativeHandle(ref, () => ({
-        refresh() {
-            if (bScroll) {
-                bScroll.refresh();
-                bScroll.scrollTo(0, 0);
-            }
-        },
-        getBScroll() {
-            if (bScroll) {
-                return bScroll;
-            }
-        }
-    }));
+    setBScroll(scroll);
+    return () => {
+      setBScroll(null);
+    }
+  }, []);
 
-    const PullupDisplayStyle = pullupLoading ? { display: "" } : { display: "" }
-    const PulldownDisplayStyle = pulldownLoading ? { display: "" } : { display: "" }
-    return (
-        <ScrollContainer ref={scrollContaninerRef}>
-            {props.children}
-            {/* <PullupLoading style={PullupDisplayStyle}><Loading></Loading></PullupLoading> */}
-            {/* <PulldownLoading style={PulldownDisplayStyle}><LoadingV2></LoadingV2></PulldownLoading> */}
-        </ScrollContainer>
-    )
-})
+  useEffect(() => {
+    if (!bScroll || !onScroll) return;
+    bScroll.on("scroll", (scroll) => {
+      onScroll(scroll);
+    });
+    return () => {
+      bScroll.off("scroll");
+    };
+  }, [onScroll, bScroll]);
+
+  useEffect(() => {
+    if (!bScroll || !pullUp) return;
+    bScroll.on('scrollEnd', () => {
+        if (bScroll.y <= bScroll.maxScrollY + 100) {
+            pullUpDebounce();
+        }
+    })
+    return () => {
+      bScroll.off("scrollEnd");
+    };
+  }, [pullUp, pullUpDebounce, bScroll]);
+
+  useEffect(() => {
+    if (!bScroll || !pullDown) return;
+    bScroll.on("touchEnd", (pos) => {
+      if (pos.y > 50) {
+        pullDownDebounce();
+      }
+    });
+
+    return () => {
+      bScroll.off("touchEnd");
+    };
+  }, [pullDown, pullDownDebounce, bScroll]);
+
+  useEffect(() => {
+    if (refresh && bScroll) {
+      bScroll.refresh();
+    }
+  });
+
+  useImperativeHandle(ref, () => ({
+    refresh() {
+      if (bScroll) {
+        bScroll.refresh();
+        bScroll.scrollTo(0, 0);
+      }
+    },
+    getBScroll() {
+      if (bScroll) {
+        return bScroll;
+      }
+    },
+  }));
+
+  const PullupDisplayStyle = pullUpLoading ? { display: "" } : { display: "" };
+  const PulldownDisplayStyle = pullDownLoading
+    ? { display: "" }
+    : { display: "" };
+  return (
+    <ScrollContainer ref={scrollContaninerRef}>
+      {props.children}
+      <PullupLoading style={PullupDisplayStyle}><Loading></Loading></PullupLoading>
+      <PullDownLoading style={PulldownDisplayStyle}><LoadingV2></LoadingV2></PullDownLoading>
+    </ScrollContainer>
+  );
+});
 
 Scroll.defaultProps = {
-    direction: "vertical",
-    click: true,
-    refresh: true,
-    onScroll: null,
-    pullupLoading: false,
-    pulldownLoading: false,
-    pullUp: null,
-    pullDown: null,
-    bounceTop: true,
-    bounceBottom: true
+  direction: "vertical",
+  click: true,
+  refresh: true,
+  onScroll: null,
+  pullUpLoading: false,
+  pullDownLoading: false,
+  pullUp: null,
+  pullDown: null,
+  bounceTop: true,
+  bounceBottom: true,
 };
 
 Scroll.propTypes = {
-    direction: PropTypes.oneOf(['vertical', 'horizental']),// 滚动的方向
-    click: PropTypes.bool,
-    refresh: PropTypes.bool,// 是否刷新
-    onScroll: PropTypes.func,// 滑动触发的回调函数
-    pullUp: PropTypes.func,// 上拉加载逻辑
-    pullDown: PropTypes.func,// 下拉加载逻辑
-    pullupLoading: PropTypes.bool,// 是否显示上拉 loading 动画
-    pulldownLoading: PropTypes.bool,// 是否显示下拉 loading 动画
-    bounceTop: PropTypes.bool,// 是否支持向上吸顶
-    bounceBottom: PropTypes.bool// 是否支持向下吸底
+  direction: PropTypes.oneOf(["vertical", "horizental"]), // 滚动的方向
+  click: PropTypes.bool,
+  refresh: PropTypes.bool, // 是否刷新
+  onScroll: PropTypes.func, // 滑动触发的回调函数
+  pullUp: PropTypes.func, // 上拉加载逻辑
+  pullDown: PropTypes.func, // 下拉加载逻辑
+  pullUpLoading: PropTypes.bool, // 是否显示上拉 loading 动画
+  pullDownLoading: PropTypes.bool, // 是否显示下拉 loading 动画
+  bounceTop: PropTypes.bool, // 是否支持向上吸顶
+  bounceBottom: PropTypes.bool, // 是否支持向下吸底
 };
 
-
-
-export default Scroll
+export default Scroll;
