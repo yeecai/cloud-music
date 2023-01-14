16605e75a4a24d6d59cb1be1456f850583ae83cf
Ye Cai <yushunchn@gmail.com>
2023-01-14 18:30:00 +0800

    update singer

--- src/application/Singer/style.js
+++ src/application/Singer/style.js
@@ -76,3 +76,18 @@ export const BgLayer = styled.div`
   border-radius: 10px;
   z-index: -50;
 `
+
+export const SongListWrapper = styled.div`
+  position: absolute;
+  z-index: 50;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  >div {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    overflow: visible;
+  }
+`
